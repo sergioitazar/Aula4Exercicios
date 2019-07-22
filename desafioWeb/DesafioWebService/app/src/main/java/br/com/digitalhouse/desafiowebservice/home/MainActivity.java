f06359c1c4b2b45cbcd56507a9b53c14fe09a97c
sergioitazar <itazar@gmail.com>
2019-07-22 19:45:52 -0300

    firebase2

---
+++ desafioWeb/DesafioWebService/app/src/main/java/br/com/digitalhouse/desafiowebservice/home/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.digitalhouse.desafiowebservice.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.desafiowebservice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//return;
+    }
+}
